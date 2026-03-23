bcbc7c1a368c272c264427a0f13fc7475dd4ad33
Rodrigo Yuji Seto Soma <rodrigo.y.seto@gmail.com>
2026-03-23 16:19:58 +0900

    feat: initialize memory game project with React setup, styling, and game card assets.

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import * as C from './App.styles';
+import logoImage from './assets/devmemory_logo.png'
+
+const App = () => {
+  return (
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={logoImage} width="200" alt="" />
+        </C.LogoLink>
+
+        <C.InfoArea>
+          ...
+        </C.InfoArea>
+
+        <button>Reset</button>
+      </C.Info>
+      <C.GridArea>
+        ...
+      </C.GridArea>
+    </C.Container>
+  );
+}
+
+export default App;
