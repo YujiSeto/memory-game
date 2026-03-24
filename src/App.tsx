eb7ba6e37c41eddc2bf68fac3af71790683c433a
Rodrigo Yuji Seto Soma <rodrigo.y.seto@gmail.com>
2026-03-24 18:54:14 +0900

    feat: Implement initial memory game structure, core components, styles, and assets.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
+import { useEffect, useState } from 'react';
 import * as C from './App.styles';
-import logoImage from './assets/devmemory_logo.png'
+import logoImage from './assets/devmemory_logo.png';
+import resetIcon from './svgs/reset.svg';
+import { InfoItem } from './components/InfoItem';
+import { Button } from './components/Button';
+import { GridItemType } from './types/GridItemType';
+import { items } from './data/items';
 
 const App = () => {
+  const [playing, setPlaying] = useState<boolean>(false);
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [moveCount, setMoveCount] = useState<number>(0);
+  const [showCount, setShowCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
+
+  useEffect(() => resetAndCreateGrid(), []);
+
+  const resetAndCreateGrid = () => {
+    setTimeElapsed(0);
+    setMoveCount(0);
+    setShowCount(0);
+
+    let tempGrid: GridItemType[] = [];
+    for (let i = 0; i < items.length * 2; i++) {
+      tempGrid.push({ item: null, shown: false, permanentShown: false });
+    }
+
+    for (let w = 0; w < 2; w++) {
+      for (let i = 0; i < items.length; i++) {
+        let pos = -1;
+        while (pos < 0 || tempGrid[pos].item !== null) {
+          pos = Math.floor(Math.random() * (items.length * 2));
+        }
+        tempGrid[pos].item = i;
+      }
+    }
+
+    setGridItems(tempGrid);
+
+    setPlaying(true);
+  };
+
   return (
     <C.Container>
       <C.Info>
@@ -10,16 +49,18 @@ const App = () => {
         </C.LogoLink>
 
         <C.InfoArea>
-          ...
+          <InfoItem label="Time" value="00:00" />
+          <InfoItem label="Moves" value="0" />
+          <InfoItem label="Matches" value="0" />
         </C.InfoArea>
 
-        <button>Reset</button>
+        <Button label="Reset" icon={resetIcon} onClick={resetAndCreateGrid} />
       </C.Info>
       <C.GridArea>
-        ...
+        <C.Grid></C.Grid>
       </C.GridArea>
     </C.Container>
   );
-}
+};
 
 export default App;
---
+++ src/data/items.ts
@@ -0,0 +1,15 @@
+import android from '../svgs/android.svg';
+import truck from '../svgs/truck.svg';
+import disney from '../svgs/disney.svg';
+import star from '../svgs/star.svg';
+import oil from '../svgs/oil.svg';
+import bike from '../svgs/bike.svg';
+
+export const items = [
+  {name: 'android', icon: android},
+  {name: 'truck', icon: truck},
+  {name: 'disney', icon: disney},
+  {name: 'star', icon: star},
+  {name: 'oil', icon: oil},
+  {name: 'bike', icon: bike}
+]
